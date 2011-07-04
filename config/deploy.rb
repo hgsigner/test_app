b9778aa7d1148a767951c4be948be9e4a88120de
Hugo Miranda <hugo@aioninnovations.com>
2011-07-04 16:14:07 -0300

    new

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,32 @@
-require 'bundler/capistrano'
-
-default_run_options[:pty] = false
-#ssh_options[:forward_agent] = true
-set :use_sudo, false
-set :user, "aionandrails"
-
-set :application, "test.aioninnovations.com"
-set :repository,  "git@github.com:hgsigner/test_app.git"
-set :scm, :git
-set :branch, 'master'
+set :application, 'test.aioninnovations.com'
+set :user, 'aionandrails'
+set :password, 'q5V3TYdf'
+set :domain, 'test.aioninnovations.com'
+set :mongrel_port, '4'
+set :mongrel_nodes, "4"
+set :rails_env,     "production"
+set :server_hostname, 'bill.dreamhost.com'
+ 
+set :git_account, 'hgsigner'
+ 
+set :scm_passphrase,  Proc.new { Capistrano::CLI.password_prompt('call me git') }
+ 
+role :web, 'test.aioninnovations.com'
+role :app, 'test.aioninnovations.com'
+role :db, 'test.aioninnovations.com', :primary => true
+ 
+default_run_options[:pty] = true
+set :repository,  "git@github.com:#{git_account}/test_app.git"
+set :scm, "git"
+set :user, user
+ 
+ssh_options[:forward_agent] = true
+set :branch, "master"
+set :deploy_via, :checkout
 set :git_shallow_clone, 1
-set :deploy_via, :remote_cache
-set :copy_compression, :bz2
-set :rails_env, 'production'
+set :git_enable_submodules, 1
+set :use_sudo, false
 set :deploy_to, "/home/#{user}/#{application}"
-
-role :web, "#{application}"                          # Your HTTP server, Apache/etc
-role :app, "#{application}"                          # This may be the same as your `Web` server
-role :db,  "#{application}", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
-
 
 
 
