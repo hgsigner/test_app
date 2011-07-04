cba58517cc69ba24aeb36cd035692c4c6440c8e8
Hugo Miranda <hugo@aioninnovations.com>
2011-07-04 14:09:23 -0300

    initial

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+require 'bundler/capistrano'
+
+default_run_options[:pty] = false
+ssh_options[:forward_agent] = true
+set :use_sudo, false
+set :user, "aionandrails"
+
+set :application, "test.aioninnovations.com"
+set :repository,  "git@github.com:hgsigner/test_app.git"
+set :scm, :git
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :deploy_via, :remote_cache
+set :copy_compression, :bz2
+set :rails_env, 'production'
+set :deploy_to, "/home/#{user}/#{application}"
+
+role :web, "#{application}"                          # Your HTTP server, Apache/etc
+role :app, "#{application}"                          # This may be the same as your `Web` server
+role :db,  "#{application}", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '60adf25cf2a2bec72d3ebbfb6ac320af807eed2aef058071f0b667fb07547c298796e178cbe779158aec87223f58f41bf9afec45340fde7c7b13df0b5a6d7f96'
